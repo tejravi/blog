9074cb17326fcf038849e292c15b74653e45675a
tejasra <56512902+tejasra@users.noreply.github.com>
2020-11-20 16:33:14 +0530

    Add files via upload
    The blog Assignment using express

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.static("./views"));
+const addUser = require("./routes/addUser");
+const users = require("./routes/users");
+const showUser = require("./routes/showUser");
+app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/new", addUser);
+app.use("/adduser", addUser);
+app.use("/show", showUser);
+app.get("/", (req, res) => {
+  res.render("index", { users: users });
+});
+
+app.listen(3000, () => {
+  console.log("Listening from port number 3000");
+});
