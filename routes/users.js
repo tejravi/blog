a5bf99ca9d20f3e20fdaed185f5e84e566199637
tejasra <56512902+tejasra@users.noreply.github.com>
2020-11-23 14:32:37 +0530

    Create users.js

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const users = [
+  {
+    id: 1,
+    description: "The Express js is the frame work used in the Node js",
+    blog: "Express js",
+    name: "Tejas R",
+  },
+];
+module.exports = users;
